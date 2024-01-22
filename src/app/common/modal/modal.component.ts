97717240a813d4754bb57f3d4189896a86486241
MingFam0111 <124756674+MingFam0111@users.noreply.github.com>
2024-01-22 16:37:18 +0700

    push

---
+++ src/app/common/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cModal',
+    templateUrl: 'modal.component.html'
+})
+
+export class ModalComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
