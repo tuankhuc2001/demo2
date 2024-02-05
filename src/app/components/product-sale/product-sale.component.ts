a155aff914eeee40537d837123a609fbea8bdd0a
linhka2k <linhdangdeveloper@gmail.com>
2024-02-05 17:18:45 +0700

    linh

--- src/app/components/product-sale/product-sale.component.ts
+++ src/app/components/product-sale/product-sale.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { DrawerService} from '../../services/drawer.service';
 @Component({
   selector: 'app-product-sale',
   templateUrl: './product-sale.component.html',
   styleUrl: './product-sale.component.css'
 })
 export class ProductSaleComponent {
+  constructor(private drawerService: DrawerService) {}
 
+  toggleDrawer(): void {
+    this.drawerService.toggleDrawer();
+  }
 }
--- src/app/components/warehouse/warehouse.component.ts
+++ src/app/components/warehouse/warehouse.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { DrawerService} from '../../services/drawer.service';
 @Component({
   selector: 'app-warehouse',
   templateUrl: './warehouse.component.html',
   styleUrl: './warehouse.component.css'
 })
 export class WarehouseComponent {
+  constructor(private drawerService: DrawerService) {}
 
+  toggleDrawer(): void {
+    this.drawerService.toggleDrawer();
+  }
 }
