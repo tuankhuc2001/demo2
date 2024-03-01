a1d038b5b6397b7df8064604706df180d7335fac
linhka2k <linhdangdeveloper@gmail.com>
2024-03-01 16:29:45 +0700

    upload

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable  } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
     constructor(private http: HttpClient) { }
-  public upload(payload: FormData): Observable<any> {
+  public upload(payload: any): Observable<any> {
     return this.http.post<any>(
       `http://localhost:8080/upload
     `,
