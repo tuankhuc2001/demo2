17dad58e557ed872fdbde6a7a144499c8ec2fd78
linhka2k <linhdangdeveloper@gmail.com>
2024-03-01 12:02:56 +0700

    upload

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,66 +1,103 @@
 import { Component } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { NzUploadFile } from 'ng-zorro-antd/upload';
 import { Observable, Observer } from 'rxjs';
 import { getISOWeek } from 'date-fns';
-
+import { NzUploadFile } from 'ng-zorro-antd/upload';
+import { ProductService } from '../../services/product.service';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css',
 })
 export class AddProductComponent {
-  constructor(private msg: NzMessageService) {}
+  constructor(private productService: ProductService, private msg: NzMessageService) {}
 
-  listCard: any = [{},{},{}]
+  formProduct: {
+    nameProduct: String;
+    quantityProduct: Number;
+    expiredDate: String;
+    provider: String;
+    unit: String;
+    origin: String;
+    avatar: any;
+    codeProduct: String;
+    description: String;
+    providePrice: Number;
+    floorPrice: Number;
+  } = {
+    nameProduct: 'abc',
+    quantityProduct: 1,
+    expiredDate: 'abc',
+    provider: 'abc',
+    unit: 'abc',
+    origin: 'abc',
+    avatar: [],
+    codeProduct: 'abc',
+    description: 'abc',
+    providePrice: 1,
+    floorPrice: 1,
+  };
+
+  fileListInfo: any = [];
+
+  listCard: any = [{}, {}, {}];
   loading = false;
   avatarUrl?: string;
   date = null;
   isEnglish = false;
-  beforeUpload = (
-    file: NzUploadFile,
-    _fileList: NzUploadFile[]
-  ): Observable<boolean> =>
-    new Observable((observer: Observer<boolean>) => {
-      const isJpgOrPng =
-        file.type === 'image/jpeg' || file.type === 'image/png';
-      if (!isJpgOrPng) {
-        this.msg.error('You can only upload JPG file!');
-        observer.complete();
-        return;
-      }
-      const isLt2M = file.size! / 1024 / 1024 < 2;
-      if (!isLt2M) {
-        this.msg.error('Image must smaller than 2MB!');
-        observer.complete();
-        return;
-      }
-      observer.next(isJpgOrPng && isLt2M);
-      observer.complete();
-    });
+
+  beforeUpload = (file: NzUploadFile): boolean => {
+    this.formProduct.avatar = this.formProduct.avatar.concat(file);
+    return false;
+  };
 
   private getBase64(img: File, callback: (img: string) => void): void {
     const reader = new FileReader();
     reader.addEventListener('load', () => callback(reader.result!.toString()));
     reader.readAsDataURL(img);
   }
-
   handleChange(info: { file: NzUploadFile }): void {
-    switch (info.file.status) {
-      case 'uploading':
-        this.loading = true;
-        break;
-      case 'done':
-        // Get this url from response in real world.
-        this.getBase64(info.file!.originFileObj!, (img: string) => {
-          this.loading = false;
-          this.avatarUrl = img;
-        });
-        break;
-      case 'error':
-        this.msg.error('Network error');
-        this.loading = false;
-        break;
+    // this.formProduct.avatar = info;
+    console.log(info.file,"abc");
+    
+    this.formProduct.avatar = [info.file];  
+    // const src = URL.createObjectURL(info)
+    // switch (info.file.status) {
+    //   case 'uploading':
+    //     this.loading = true; 
+    //     break;
+    //   case 'done':
+    //     // Get this url from response in real world.
+    //     
+    //     this.getBase64(info.file!.originFileObj!, (img: string) => {
+    //       this.loading = false;
+    //       this.avatarUrl = img;
+    //     });
+    //     break;
+    //   case 'error':
+    //     this.msg.error('Network error');
+    //     this.loading = false;
+    //     break;
+    // }
+  }
+  test(file:any){
+    this.formProduct.avatar = file;
+    console.log(file,"file");
+    
+  }
+
+  handleCallAIP() {
+    console.log(this.formProduct,"abc");
+    const formData = new FormData();
+    this.formProduct.avatar.forEach((file: any) => {
+      formData.append('file', file);
+    })
+    this.loading = true;
+    this.productService.upload(formData).subscribe(() =>{
+      this.loading = false
+      this.formProduct.avatar = [];
+      this.msg.success('upload successfully.');
     }
+    );
   }
 }
