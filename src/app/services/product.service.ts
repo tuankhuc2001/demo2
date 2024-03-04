2c44648b3984044a0c87a4ec10271c1659426381
MingFam0111 <124756674+MingFam0111@users.noreply.github.com>
2024-03-04 17:12:59 +0700

    push develop

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IResponseProduct } from '../types/product';
 import { apiProduct } from '../constant/api';
 
