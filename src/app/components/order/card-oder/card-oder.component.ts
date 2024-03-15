0efcccb4927dfd23f3ff4811076d917300d707a7
nhatminh25 <hnminh020501@gmail.com>
2024-03-15 16:46:12 +0700

    fix state order

--- src/app/components/order/card-oder/card-oder.component.ts
+++ src/app/components/order/card-oder/card-oder.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IOrder } from '../../../types/order';
 
 @Component({
   selector: 'app-card-oder',
@@ -6,7 +7,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './card-oder.component.css',
 })
 export class CardOderComponent {
-  @Input() listCard: any;
+  @Input() listCard: IOrder[] = [];
+
   @Output() onClickCard: EventEmitter<number> = new EventEmitter()
 
   handleClickCard(item: number){
