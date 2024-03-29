de752c836ce661dc050dadcf8375cf590e01c448
MingFam0111 <124756674+MingFam0111@users.noreply.github.com>
2024-03-29 18:20:46 +0700

    u

--- src/app/pages/side-bar/side-bar.component.ts
+++ src/app/pages/side-bar/side-bar.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { routerNames } from '../../constant/router';
 import { SearchService } from '../../services/search.service';
 import { UserService } from '../../services/user.service';
+import { IUser } from '../../types/user';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
   constructor(private router: Router, private searchService: SearchService, private userService: UserService) { }
 
   routerNames = routerNames
-  user = this.userService.getUser()
+  user: IUser = {
+    id: 0,
+    phone: 'string',
+    email: 'string',
+    fullname: 'string',
+    avatar: 'any',
+    role: 'string',
+    token: 'string',
+    refreshToken: 'string'
+  }
 
   sideDrawerVisible = false;
   currentSubMenu: string | null = null;
-  nameUser: string = "ADMIN"
-  role: string = "Admin"
-  phone: string = "0987654321"
+  nameUser: string = this.user.fullname
+  role: string = this.user.role
+  phone: string = this.user.phone
   textSearch: string = ""
 
   handleSearch(e: any) {
@@ -40,6 +50,11 @@ export class SideBarComponent {
   }
 
   ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next: (res: IUser) => {
+        this.user = res
+      }
+    })
 
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { IProduct, IRequestProduct, IResponseProduct } from '../types/product';
 import { apiProduct } from '../constant/api';
-import { NzUploadFile } from 'ng-zorro-antd/upload';
 import { UserService } from './user.service';
 
 
